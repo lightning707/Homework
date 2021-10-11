a90d48d9f615eb4490260006f334d18b91a19b59
lightning707 <andrew@bortnik.org>
2021-10-11 17:36:53 +0300

    Lesson 9 homework

---
+++ 1_9/1_9_1_read.py
@@ -0,0 +1,4 @@
+with open("myfile.txt", 'r') as file:
+    print(file.read())
+
+# Script reads file from directory where it is run
---
+++ 1_9/1_9_1_write.py
@@ -0,0 +1,9 @@
+with open("myfile.txt", "a") as file:
+    file.write("Hello file world!\n")
+
+# Script creates file in the directory where it is run
+
+#with open("Homework/1_9/myfile.txt", "a") as file:
+#    file.write("Hello file world! (2)\n")
+
+# Script creates file in the directory "Homework/1_9/ if it exists
