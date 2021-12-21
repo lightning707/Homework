291a72863f676ff0faf6170dc5086ee6f0463641
lightning707 <andrew@bortnik.org>
2021-12-21 20:20:15 +0200

    Lesson 34 homework

--- 4_34/4_34_1.py
+++ 4_34/4_34_1.py
@@ -1,9 +1,11 @@
 import requests
 import pprint
+import json
 
 params = {
     'subreddit': 'pathofexile',
-    'size': 100
+    'size': 100,
+    'sort_type': 'created_utc'
 }
 
 resp = requests.get('https://api.pushshift.io/reddit/search/comment', params=params)
@@ -12,5 +14,8 @@ print(resp.status_code)
 data = resp.json()['data']
 # print(data)
 
-comment_list = sorted(list(set([comm.get('body') for comm in data])))
+comment_list = [comm.get('body') for comm in data]
 pprint.pprint(comment_list)
+
+with open('reddit.json', 'w') as fd:
+    json.dump(comment_list, fd)
